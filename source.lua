976537b290b93b45b926bed4d3f8abe3000f597e
abstraact <108076105+agreeed@users.noreply.github.com>
2023-08-26 14:02:23 +0500

    edited title

--- source.lua
+++ source.lua
@@ -206,7 +206,7 @@ Title.BorderSizePixel = 0
 Title.Size = UDim2.new(0, 250, 0, 20)
 Title.Font = Enum.Font.SourceSans
 Title.TextSize = 18
-Title.Text = "Infinite Yield FY"
+Title.Text = "Infinite Yield FY (fuck you)"
 
 do
 	local emoji = ({
